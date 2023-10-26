9f5b4b8a7c42c0492824afffdf2ebbcd2e500e9e
Byashpaswan <byas.paswan99@gmail.com>
2023-10-26 09:57:52 +0530

    full stack smart contact manager

---
+++ src/main/java/com/Byas/SCM/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.Byas.SCM.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.Byas.SCM.entity.User;
+
+public interface UserRepository  extends JpaRepository<User,Integer>{
+	 
+	@Query("select u from User u where u.email=:email")
+	public User getUserByUserName(@Param("email") String email);
+
+}
